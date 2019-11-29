d023ca94f7fd525c98a02bf2cd04a35979a254ac
Romina Bazan <danielabazan94@gmail.com>
2019-11-29 19:17:45 -0300

    actualizo stage_view

--- elearning_stages/__manifest__.py
+++ elearning_stages/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'elearning_stages',
     'description': 'Módulo que agrega estados del curso.',
     'author': 'Comunidad Odoo Misiones',
-    'depends': ['base','website_slides','elearning_teacher'],
+    'depends': ['base','website_slides'],
     'application': True,
     'data': ['views/stage_menu.xml','views/stage_view.xml']
 }
--- elearning_stages/models/elearning_stage.py
+++ elearning_stages/models/elearning_stage.py
@@ -3,7 +3,7 @@ from odoo import fields, models
 
 class elearningStage(models.Model):
     _name = "elearning.stage"
-    _description = "elearning.stage"
+    _description = "Elearning Stage Description"
     
-    name = fields.Char(help="What's name?")
+    name = fields.Char(help="What's name?",required = True)
     description= fields.Char(help="Give me a good description!")
