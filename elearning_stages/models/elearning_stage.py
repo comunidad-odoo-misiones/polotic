ffd331b3d5846b3c0734f9ab63877fb6fb3e7a73
Romina Bazan <danielabazan94@gmail.com>
2019-11-29 16:45:59 -0300

    actualizo archivos y clase heredada

--- elearning_stages/models/elearning_stage.py
+++ elearning_stages/models/elearning_stage.py
@@ -4,5 +4,6 @@ from odoo import fields, models
 class elearningStage(models.Model):
     _name = "elearning.stage"
     _description = "elearning.stage"
+    
     name = fields.Char(help="What's name?")
     description= fields.Char(help="Give me a good description!")
