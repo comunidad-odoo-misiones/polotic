0f6b5ed7ac28a49e1b397c2e03338582c86e7dee
Lgpicek <leandropicek231@gmail.com>
2019-11-29 11:57:28 -0300

    Va para el testeo

---
+++ elearning_stages/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'elearning_stages',
+    'description': 'Módulo que agrega estados del curso.',
+    'author': 'Comunidad Odoo Misiones',
+    'depends': ['base','website_slides','elearning_teacher'],
+    'application': True,
+    'data': ['views/stage_menu.xml','views/stage_view.xml']
+}
