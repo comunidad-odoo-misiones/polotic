55c7cc3d4e3bb4e5f38c8094a995609022d09cbf
Romina Bazan <danielabazan94@gmail.com>
2019-11-29 20:33:43 -0300

    actualice con STATUSBAR

--- elearning_stages/models/elearning_stage.py
+++ elearning_stages/models/elearning_stage.py
@@ -7,3 +7,6 @@ class elearningStage(models.Model):
     
     name = fields.Char(help="What's name?",required = True)
     description= fields.Char(help="Give me a good description!")
+
+    state = fields.Selection([('draft', 'Draft'), ('done', 'Done'),
+   		('cancel', 'Cancelled'), ], required=True, default='draft')
